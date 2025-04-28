7c494611c046839672a75fbbd910419e6e9d63b2
xiao278 <90475159+xiao278@users.noreply.github.com>
2025-04-28 13:57:18 -0400

    added clustering visualization
    additionlly removed unused code in community_detection_implementation.ipynb

--- graphing_helper.py
+++ graphing_helper.py
@@ -12,7 +12,7 @@ def get_mappings(G:networkx.Graph):
     return (node_to_index, index_to_node)
 
 def bipartite_clustered_positioning(G:networkx.Graph, INDEX:list[int]):
-    top_nodes, bottom_nodes = networkx.algorithms.bipartite.sets(G)
+    (top_nodes, bottom_nodes) = networkx.algorithms.bipartite.sets(G)
     (node_to_index, index_to_node) = get_mappings(G)
 
     top_node_index = {}
@@ -56,17 +56,39 @@ def bipartite_clustered_positioning(G:networkx.Graph, INDEX:list[int]):
 
     return pos
 
-def get_colormappings(G:networkx.Graph, INDEX:list[int], clusters:list[dict]):
+def spiral_clustered_positioning(G:networkx.Graph, INDEX:list[int], resolution=0.35):
+    (node_to_index, index_to_node) = get_mappings(G)
+    reordered_graph = networkx.Graph()
+    new_nodes = []
+    for index in INDEX:
+        new_nodes.append(index_to_node[index])
+    reordered_graph.add_nodes_from(new_nodes)
+    reordered_graph.add_edges_from(G.edges())
+    pos = networkx.spiral_layout(reordered_graph, resolution=resolution)
+    return pos
+
+
+def get_colormappings(G:networkx.Graph, INDEX:list[int], clusters:list[dict], cmap_name='Set3'):
     (node_to_index, index_to_node) = get_mappings(G)
     group_labels = {}
     for node in G.nodes:
         group_labels[node] = 0
-    for index_of_INDEX in clusters[0]:
-        index = INDEX[index_of_INDEX]
-        node = index_to_node[index]
-        group_labels[node] = 1
+    for i in range(len(clusters)):
+        for index_of_INDEX in clusters[i]:
+            index = INDEX[index_of_INDEX]
+            node = index_to_node[index]
+            group_labels[node] = i + 1
     unique_groups = set(group_labels.values())
-    colors = matplotlib.cm.get_cmap("Pastel1", len(unique_groups))
-    color_map = {group: colors(i) for i, group in enumerate(unique_groups)}
-    node_colors = [color_map[group_labels[node]] for node in G.nodes]
-    return node_colors
+    colors = matplotlib.cm.get_cmap(cmap_name, len(unique_groups))
+    color_map = {group: colors(i) for i, group in enumerate(unique_groups)} # color assigned to each group label
+    node_colors = ['lightgray' if group_labels[node] == 0 else color_map[group_labels[node]] for node in G.nodes]
+    edge_colors = []
+    for u, v in G.edges():
+        u_group = group_labels[u]
+        v_group = group_labels[v]
+        if u_group != 0 and v_group != 0 and u_group == v_group:
+            edge_colors.append(color_map[u_group])
+        else:
+            edge_colors.append(None)
+
+    return (node_colors, edge_colors)
